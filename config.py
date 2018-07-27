430dc2e3ce62db9abce180ab25d4d3931df8e137
JBIsaik <41596157+JBIsaik@users.noreply.github.com>
2018-07-27 17:01:05 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 token = 'NDcxOTUyODk5NzgyNjA2ODQ4.DjsVGg.cAyaEStZpbwwNUMO2jskGdKg6K8'
 
-prefix = '^' #put prefix here
+prefix = '%' #put prefix here
 
 link = 'https://discordapp.com/api/oauth2/authorize?client_id=472073031770963978&permissions=8&scope=bot'
 
